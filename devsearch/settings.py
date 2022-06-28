2cdcc70adc5ab90b5e9efce819cc0bd8ef80abf6
ahnge <ahnge226@gmail.com>
2022-06-28 22:09:41 +0630

    📧 send email when new user register
    👉 configure email host, ..etc for smtp email service
    👉 EMAIL_HOST_PASSWORD is included in settings but I've already deleted it. Will setup env variables when deploy.
    👉 send mail with django.core.mail api when register signal trigger.

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -119,6 +119,14 @@ USE_I18N = True
 USE_TZ = True
 
 
+EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_HOST_USER = 'ahnge226@zohomail.com'
+EMAIL_HOST_PASSWORD = 'H4GAize20T2Q'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
