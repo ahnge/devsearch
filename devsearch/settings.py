c5b4c7abbc960b60542db84a59986c2fdf3a4f10
Nay Zaw Min Naing <62560191+ahnge@users.noreply.github.com>
2022-06-29 10:00:08 +0630

    Update settings
    delete app password for smtp server

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 EMAIL_HOST = 'smtp.zoho.com'
 EMAIL_HOST_USER = 'ahnge226@zohomail.com'
-EMAIL_HOST_PASSWORD = 'H4GAize20T2Q'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
