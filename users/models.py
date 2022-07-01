f64939a926e157d94f18f2ec371e6e1fd7993878
ahnge <ahnge226@gmail.com>
2022-07-01 12:39:32 +0630

    🐛 fix profile img bug

--- users/models.py
+++ users/models.py
@@ -26,6 +26,14 @@ class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
 
+    @property
+    def p_img_url(self):
+        try:
+            url = self.profile_image.url
+        except:
+            url = "/images/profiles/user-default.png"
+        return url
+
     def __str__(self):
         return self.username
 
